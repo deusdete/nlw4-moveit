713f4e1f40e3f5a6433026f04a8235eb7b444fa3
Deusdete Ferreira <deuzinh2010@gmail.com>
2021-02-24 07:31:13 -0300

    Init commit

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,11 @@
+export function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0 px</span>
+      <div>
+        <div></div>
+      </div>
+      <span>600 px</span>
+    </header>
+  )
+}
